6702a4aa8600d5988b9953a15f4e8f71bc2993bf
Richard <Richard@192.168.1.103>
2012-10-23 15:35:13 -0700

    Can detect when day selected on calendar in event creation window.

--- Leviathan/src/stuffplotter/UI/MapCalendarView.java
+++ Leviathan/src/stuffplotter/UI/MapCalendarView.java
@@ -1,8 +1,13 @@
 package stuffplotter.UI;
 
+import java.util.Date;
+
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.maps.client.MapWidget;
 import com.google.gwt.maps.client.control.LargeMapControl3D;
 import com.google.gwt.maps.client.geom.LatLng;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.TabPanel;
@@ -26,7 +31,24 @@ public class MapCalendarView extends SimplePanel
 		map.addControl(new LargeMapControl3D());
 		mapCalHolder.add(map, new Label("Map"));
 		DatePicker calendar = new DatePicker();
+		initializeCalendarChangeHandler(calendar);
 		mapCalHolder.add(calendar, new Label("Calendar"));
 		this.add(mapCalHolder);
+	}
+	
+	/**
+	 * Helper method to initialize the change handler for the calendar.
+	 * @param calendar - the calendar to add the change handler to.
+	 */
+	private void initializeCalendarChangeHandler(DatePicker calendar)
+	{
+		calendar.addValueChangeHandler(new ValueChangeHandler<Date>()
+		{
+			@Override
+			public void onValueChange(ValueChangeEvent<Date> event)
+			{
+				Window.alert(event.getValue().toString());
+			}			
+		});
 	}
 }
