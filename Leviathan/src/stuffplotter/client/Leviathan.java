fcba7be6e9ad6710f79df70de6120e54400a6c81
Richard <Richard@192.168.1.103>
2012-11-09 20:17:01 -0800

    Removed most test code from the Leviathan file.  Confirmed proper date can be returned from calendar.

--- Leviathan/src/stuffplotter/client/Leviathan.java
+++ Leviathan/src/stuffplotter/client/Leviathan.java
@@ -1,20 +1,14 @@
 package stuffplotter.client;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import stuffplotter.UI.AccountPanel;
 import stuffplotter.UI.AvailabilitySubmitterDialogBox;
 import stuffplotter.UI.EventCreationDialogBox;
-import stuffplotter.UI.EventLocationSearchPanel;
 import stuffplotter.UI.FriendFinderDialogBox;
-import stuffplotter.UI.MonthPanel.Month;
 import stuffplotter.UI.TopRightPanel;
 import stuffplotter.shared.Account;
-import stuffplotter.shared.DayContainer;
-import stuffplotter.shared.Event;
-import stuffplotter.shared.MonthContainer;
 
 import com.bradrydzewski.gwt.calendar.client.Calendar;
 import com.bradrydzewski.gwt.calendar.client.CalendarViews;
@@ -33,9 +27,7 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.shared.SimpleEventBus;
-import com.google.gwt.maps.client.MapWidget;
-import com.google.gwt.maps.client.control.LargeMapControl3D;
-import com.google.gwt.maps.client.geom.LatLng;
+import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
@@ -54,9 +46,6 @@ public class Leviathan implements EntryPoint
 	
 	private final String redirectUrl = (GWT.isProdMode()) ? GWT.getHostPageBaseURL() : GWT.getHostPageBaseURL() + "Leviathan.html?gwt.codesvr=127.0.0.1:9997";
 	private Account account = null;
-	private int friendCount = 0;
-	
-	private Long testEventId;
 	
 	/**
 	 * This is the entry point method.
@@ -102,172 +91,23 @@ public class Leviathan implements EntryPoint
 			@Override
 			public void onTimeBlockClick(TimeBlockClickEvent<Date> event)
 			{
-				// TODO Auto-generated method stub
-				Window.alert(Integer.toString(event.getTarget().getDate()));
+				DateTimeFormat dayFormat = DateTimeFormat.getFormat("MMMM,d,yyyy");
+				String[] calendarValues = dayFormat.format(event.getTarget()).toString().split(",");
+				Window.alert(calendarValues[1]);
 			}
 		});
-				
-		// testing Toy Level System
-		final LevelSystem lvlSys = new LevelSystem();
-
-		VerticalPanel lvlView = new VerticalPanel();
-		final Button addExpBtn = new Button("Up Vote");
-
-		final Label lvlLabel = new Label("Level: 1");
-		final Label expLabel = new Label("Experience: 0");
-		
-		lvlView.add(lvlLabel);
-		lvlView.add(expLabel);
-		lvlView.add(addExpBtn);
-		
-		addExpBtn.addClickHandler(new ClickHandler()
-		{
-			@Override
-			public void onClick(ClickEvent event)
-			{
-				lvlSys.addExperience(50);
-				expLabel.setText("Experience: " + Integer.toString(lvlSys.getCurrentExperience()));
-				lvlLabel.setText("Level: " + Integer.toString(lvlSys.getCurrentLevel()));
-			}	
-		});
-		
-		final Button availBtn = new Button("Test Setup Scheduler (also submit availability UI)");
-		availBtn.addClickHandler(new ClickHandler()
-		{
-			@Override
-			public void onClick(ClickEvent event)
-			{
-				AvailabilitySubmitterDialogBox availSubmitter = new AvailabilitySubmitterDialogBox(account.getUserName());
-				availSubmitter.show();
-			}
-		});
-		
+						
 		final Button createEventBtn = new Button("Create Event");
 		createEventBtn.addClickHandler(new ClickHandler()
 		{
+			EventCreationDialogBox eventCreation = null;
 			@Override
 			public void onClick(ClickEvent event)
 			{
-				EventCreationDialogBox eventCreation = new EventCreationDialogBox(account);
+				 eventCreation = new EventCreationDialogBox(account);
 			}
 		});
 		
-		final Button findFriends = new Button("Find Friends");
-		
-		
-		//Testing for adding friends and accepting
-		final TextBox testInput = new TextBox();
-		final Button addFriends = new Button("Add Friends");
-		
-		addFriends.addClickHandler(new ClickHandler()
-		{
-
-			@Override
-			public void onClick(ClickEvent event) {
-				
-				
-				AccountServiceAsync accountService = GWT.create(AccountService.class);
-				accountService.addFriend(account, testInput.getText(), new AsyncCallback<Void>(){
-
-					@Override
-					public void onFailure(Throwable caught) {
-						Window.alert("Failure");
-						
-					}
-
-					@Override
-					public void onSuccess(Void result) {
-						Window.alert("Friend Request to "+testInput.getText()+" has been added.");
-						
-					}
-					
-				});
-				
-			}
-			
-		}
-		);
-		
-		final Button getPending = new Button("See Requests");
-		getPending.addClickHandler(new ClickHandler()
-		{
-
-			@Override
-			public void onClick(ClickEvent event) {
-				
-				
-				AccountServiceAsync accountService = GWT.create(AccountService.class);
-				accountService.getPendingFriends(account, new  AsyncCallback<List<String>>(){
-
-					@Override
-					public void onFailure(Throwable caught) {
-						Window.alert("Failure");
-						
-					}
-
-					@Override
-					public void onSuccess(List<String> result) {
-						String pending = "";
-						for(String user : result){
-							pending = pending + user + "\n";
-						}
-						
-						Window.alert(pending);
-						
-					}
-					
-				});
-				
-				
-				
-			}
-			
-		}
-		);
-		
-		final Button acceptPending = new Button("Accept Request");
-		
-		acceptPending.addClickHandler(new ClickHandler()
-		{
-
-			@Override
-			public void onClick(ClickEvent event) {
-				AccountServiceAsync accountService = GWT.create(AccountService.class);
-				accountService.confirmFriendReq(account, testInput.getText(), new  AsyncCallback<Void>()
-						{
-
-							@Override
-							public void onFailure(Throwable caught) {
-								Window.alert("Failure");
-								
-							}
-
-							@Override
-							public void onSuccess(Void result) {
-								Window.alert(testInput.getText()+" has been successfully added in your friends list.");
-								
-							}
-						
-						});
-				
-			}
-			
-		});
-		
-		//END OF TEST
-		
-
-		findFriends.addClickHandler(new ClickHandler()
-		{
-			@Override
-			public void onClick(ClickEvent event)
-			{
-				FriendFinderDialogBox friendDialog = new FriendFinderDialogBox();
-				friendDialog.show();
-				
-				
-			}	
-		});
 /*		
 		// test code to read from user's Google Calendar
 		final com.google.api.gwt.services.calendar.shared.Calendar testCalendar = GWT.create(com.google.api.gwt.services.calendar.shared.Calendar.class);
@@ -314,8 +154,6 @@ public class Leviathan implements EntryPoint
 			}	
 		});
 	*/	
-		// testing user account panel
-		AccountPanel userAccountPanel = new AccountPanel(account);
 		
 		// testing top right panel for logged in user
 		TopRightPanel topRightPanel = new TopRightPanel(account);
@@ -324,15 +162,7 @@ public class Leviathan implements EntryPoint
 		calMapHolder.add(calendar);
 		
 		RootPanel.get("calMapContainter").add(calMapHolder);
-		RootPanel.get("addExp").add(lvlView);
 		RootPanel.get("eventCreation").add(createEventBtn);
-		RootPanel.get("availSub").add(availBtn);
-		RootPanel.get("friendFinder").add(findFriends);
-		RootPanel.get("friendFinder").add(testInput);
-		RootPanel.get("friendFinder").add(addFriends);
-		RootPanel.get("friendFinder").add(getPending);
-		RootPanel.get("friendFinder").add(acceptPending);
-		RootPanel.get("userAccount").add(userAccountPanel);
 		RootPanel.get("topRightPanel").add(topRightPanel);
 	}
 }
