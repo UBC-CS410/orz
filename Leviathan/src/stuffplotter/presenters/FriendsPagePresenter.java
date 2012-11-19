54d9e317413d1d161c95789069f602d2866d9161
Richard <Richard@192.168.1.103>
2012-11-18 23:47:33 -0800

    Added code to see why FriendsView wasn't getting added.

--- Leviathan/src/stuffplotter/presenters/FriendsPagePresenter.java
+++ Leviathan/src/stuffplotter/presenters/FriendsPagePresenter.java
@@ -1,8 +1,12 @@
 package stuffplotter.presenters;
 
+import java.util.Iterator;
+
 import com.google.gwt.event.dom.client.HasClickHandlers;
 import com.google.gwt.event.shared.HandlerManager;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.HasWidgets;
+import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 
 import stuffplotter.client.services.ServiceRepository;
@@ -56,6 +60,27 @@ public class FriendsPagePresenter implements Presenter
 	@Override
 	public void go(HasWidgets container)
 	{
+		Iterator<Widget> asdf = container.iterator();
+		int count = 0;
+		while(asdf.hasNext())
+		{
+			asdf.next();
+			count++;
+		}
+		
+		Window.alert("Count: " + String.valueOf(count));
+		// Doesn't seem to add FriendsView...
 		container.add(this.friendsView.asWidget());
+		container.add(new SimplePanel());
+		
+		Iterator<Widget> asdf2 = container.iterator();
+		int count2 = 0;
+		while(asdf2.hasNext())
+		{
+			asdf2.next();
+			count2++;
+		}
+		
+		Window.alert("Count: " + String.valueOf(count2));
 	}
 }
