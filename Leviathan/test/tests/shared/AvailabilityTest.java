40022182a3a61b7eb3b772561cc07a91e929409e
farezv <farezvad@gmail.com>
2012-11-22 00:04:23 -0800

    Added some more test files. Need to find workaround for private methods and fields.

---
+++ Leviathan/test/tests/shared/AvailabilityTest.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package tests.shared;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+import stuffplotter.shared.Availability;
+
+/**
+ * 
+ *
+ */
+public class AvailabilityTest
+{
+
+	
+	@Test
+	public void testAvailabilityCtor()
+	{
+		Date time = new Date();
+		int votes = 0;
+		
+		Availability av = new Availability(time);
+		
+		assertNotNull(av);
+		assertEquals(time, time);
+		assertEquals(votes, 0);
+	}
+
+	@Test
+	public void testIncrementVote() {
+		int voteinc = 1;
+		Date time = new Date();
+		
+		Availability av = new Availability(time);
+		av.incrementVote();
+		
+		//assertEquals(voteinc, )
+	}
+}
