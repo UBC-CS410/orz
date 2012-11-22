bb79d05790b6c8894964b6eb3bfb43cc17a20ffe
Allen Zhou <Allen Zhou@ZYL-PC>
2012-11-21 21:29:55 -0800

    binded submit availability button and added comment button and comment box

--- Leviathan/src/stuffplotter/presenters/EventViewPresenter.java
+++ Leviathan/src/stuffplotter/presenters/EventViewPresenter.java
@@ -2,7 +2,10 @@ package stuffplotter.presenters;
 
 import stuffplotter.client.services.ServiceRepository;
 import stuffplotter.shared.Event;
+import stuffplotter.views.events.AvailabilitySubmitterDialogBox;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.HasClickHandlers;
 import com.google.gwt.event.shared.HandlerManager;
 import com.google.gwt.user.client.ui.HasWidgets;
@@ -13,6 +16,8 @@ public class EventViewPresenter implements Presenter
 	public interface EventViewer
 	{
 		public HasClickHandlers getVoteButton();
+		public HasClickHandlers getCommentButton();
+		public void openCommentBox();
 		public Widget asWidget();
 	}
 	
@@ -29,7 +34,15 @@ public class EventViewPresenter implements Presenter
 	
 	public void bind()
 	{
-		
+		this.eventView.getVoteButton().addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event)
+			{
+				AvailabilitySubmitterDialogBox submitter = new AvailabilitySubmitterDialogBox(null);
+			}
+			
+		});
 	}
 
 	@Override
--- Leviathan/src/stuffplotter/views/events/EventView.java
+++ Leviathan/src/stuffplotter/views/events/EventView.java
@@ -10,12 +10,15 @@ import com.google.gwt.event.dom.client.HasClickHandlers;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class EventView extends VerticalPanel implements EventViewer
 {
 	private Event event;
 	private final Button voteButton;
+	private final Button commentButton;
+	private final TextBox commentBox;
 	
 	/**
 	 * Constructor for EventView.
@@ -26,6 +29,8 @@ public class EventView extends VerticalPanel implements EventViewer
 	{
 		this.event = event;
 		this.voteButton = new Button("Choose availabilities");
+		this.commentButton = new Button("Submit comment");
+		this.commentBox = new TextBox();
 		initializeView();
 	}
 
@@ -33,6 +38,19 @@ public class EventView extends VerticalPanel implements EventViewer
 	public HasClickHandlers getVoteButton()
 	{
 		return this.voteButton;
+	}
+	
+	@Override
+	public HasClickHandlers getCommentButton()
+	{
+		return this.commentButton;
+	}
+	
+	@Override
+	public void openCommentBox()
+	{
+		this.remove(this.commentButton);
+		this.add(this.commentBox);
 	}
 
 	private void initializeView()
@@ -56,6 +74,7 @@ public class EventView extends VerticalPanel implements EventViewer
 		this.add(new Anchor(where));
 		this.add(new Anchor(why));
 		
+		this.add(this.commentButton);
 		VerticalPanel wall = new VerticalPanel();
 		//TODO: add comments
 		this.add(wall);
