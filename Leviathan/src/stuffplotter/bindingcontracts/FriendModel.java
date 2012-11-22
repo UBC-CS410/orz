6cd7096c318086add725c22245c6bc846a1e01c6
Richard <Richard@192.168.1.103>
2012-11-21 20:09:23 -0800

    Added place holder interface (contract) for Friend model to views data binding.

---
+++ Leviathan/src/stuffplotter/bindingcontracts/FriendModel.java
@@ -0,0 +1,10 @@
+package stuffplotter.bindingcontracts;
+
+/**
+ * Interface used by the Friend object to enforce a contract between the model and views to
+ * simulate data binding.
+ */
+public interface FriendModel
+{
+
+}
