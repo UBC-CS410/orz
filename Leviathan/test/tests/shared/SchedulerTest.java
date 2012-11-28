242d1cbecc2106b27b9ac0ed77d5e94882d0ab50
farezv <farezvad@gmail.com>
2012-11-27 19:21:08 -0800

    Tested addAvailability()

--- Leviathan/test/tests/shared/SchedulerTest.java
+++ Leviathan/test/tests/shared/SchedulerTest.java
@@ -6,11 +6,13 @@ package tests.shared;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 import junit.framework.TestCase;
 
 import org.junit.Test;
 
+import stuffplotter.shared.Availability;
 import stuffplotter.shared.Scheduler;
 
 /**
@@ -26,9 +28,24 @@ public class SchedulerTest extends TestCase
 		ArrayList<Long> availabilities = new ArrayList<Long>();
 		
 		Scheduler sch = new Scheduler();
-		
+		Long id = null;
 		assertNotNull(sch);
 		assertEquals(availabilities, sch.getAvailabilities());
+		assertEquals(id,sch.getId());
 	}
 
+	@Test
+	public void testAddAvail()
+	{
+		ArrayList<Long> availabilities = new ArrayList<Long>();
+		
+		Scheduler sch = new Scheduler();
+		Date day = new Date();
+		Date day2 = new Date();
+		Availability pAvailability = new Availability(day);
+		Availability pAvailability2 = new Availability(day2);
+		
+		sch.addAvailability(pAvailability);
+		assertEquals(2, sch.getAvailabilities().size());
+	}
 }
